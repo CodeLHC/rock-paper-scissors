998c99a7ae6a111de8494640d62150febf5e7a82
CodeLHC <lhcrawford97@gmail.com>
2023-09-18 09:58:50 +0100

    updates to display text in html divs

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
-const rock = 'rock'
-const scissors = 'scissors'
-const paper = 'paper'
+const rock = 'rock';
+const scissors = 'scissors';
+const paper = 'paper';
 
-let playerScore = 0
-let computerScore = 0
+const choices = [rock, paper, scissors];
 
-const choices = [rock, paper, scissors]
+let playerScore = 0;
+let computerScore = 0;
 
-const toTitleCase = (word) => word[0].toUpperCase() + word.slice(1)
+const toTitleCase = (word) => word[0].toUpperCase() + word.slice(1);
 
-function getComputerChoice () {
-  return choices[(Math.floor(Math.random() * choices.length))]
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+  // seperate into variable to make it easier to read
 }
+//const computerSelection = getComputerChoice();
 
-function playRound (playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase()
-  if ((playerSelection === rock && computerSelection === scissors) || (playerSelection === paper && computerSelection === rock) || (playerSelection === scissors && computerSelection === paper)) {
-    playerScore = playerScore+1
-    return `You Win! ${toTitleCase(playerSelection)} beats ${computerSelection}`
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (
+    (playerSelection === rock && computerSelection === scissors) ||
+    (playerSelection === paper && computerSelection === rock) ||
+    (playerSelection === scissors && computerSelection === paper)
+  ) {
+    playerScore = playerScore + 1;
+    return `You Win! ${toTitleCase(
+      playerSelection
+    )} beats ${computerSelection}`;
   } else if (playerSelection === computerSelection) {
-  
-    return `It's a draw, both picked ${computerSelection}`
+    return `It's a draw, both picked ${computerSelection}`;
   }
-  computerScore = computerScore+1
-  return `You Lose! ${toTitleCase(computerSelection)} beats ${playerSelection}`
-}
-const computerSelection = getComputerChoice()
-
-
-function game () {
-for (let i = 0; i<5; i++){
-  let playerSelection = prompt("Rock, Paper or Scissors?")
-
- const result = playRound (playerSelection, computerSelection)
-  console.log(`${result}
-  Current score:
-  Player: ${playerScore}    Computer: ${computerScore}`)
+  computerScore = computerScore + 1;
+  return `You Lose! ${toTitleCase(computerSelection)} beats ${playerSelection}`;
 }
 
-if(playerScore > computerScore){
-  console.log("You won! Congrats")
-} else if (computerScore === playerScore){
-  console.log("It's a draw!")
-}
-else if (computerScore> playerScore){console.log("Computer won! Better luck next time")}
-}
+const showRound = document.getElementById('showRound');
+const showScores = document.getElementById('showScores');
+const showFinalResult = document.getElementById('showFinalResult');
 
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach((currentbtn) => {
+  currentbtn.addEventListener('click', (e) => {
+    const roundResult = playRound(e.target.innerHTML, getComputerChoice());
 
-
-console.log(game())
+    showRound.innerText = roundResult;
+    const scoreContent = `Current score:
+   Player: ${playerScore}    Computer: ${computerScore}`;
+    showScores.innerText = scoreContent;
+    if (playerScore === 5) {
+      showFinalResult.innerText = 'You won! Congrats!';
+    } else if (computerScore === 5) {
+      showFinalResult.innerText = 'Computer won! Better luck next time.';
+    }
+  });
+});
